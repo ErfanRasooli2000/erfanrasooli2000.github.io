814e198a5d053c5810290e0a4211c40252b7fe34
ErfanRasooli2000 <landa.cloner1379@gmail.com>
2021-12-20 03:22:51 +0330

    hovers and java scripts

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,35 @@ function openNavs()
         document.getElementById('nav-slider').classList.add('navigation-slider-close');
     }
 }
+
+function showAllImges(name)
+{
+    var all = document.getElementsByClassName('work-img');
+    if (name!='all')
+    {
+        var chosen = document.getElementsByClassName('work-img ' + name)
+    }
+    else
+    {
+        var chosen = all;
+    }
+
+    let size_all = all.length;
+    let size_chosen = chosen.length;
+    for (let i=0 ; i < size_all ; i++)
+    {
+        all[i].style.display='none';
+    }
+    for (let i=0 ; i < size_chosen ; i++)
+    {
+        console.log('g');
+        chosen[i].style.display='inline-block';
+    }
+
+    document.getElementById('deve').classList.remove('shadow');
+    document.getElementById('photo').classList.remove('shadow');
+    document.getElementById('plugin').classList.remove('shadow');
+    document.getElementById('design').classList.remove('shadow');
+    document.getElementById('all').classList.remove('shadow');
+    document.getElementById(name).classList.add('shadow');
+}
