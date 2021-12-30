b900c3597980175862ef66d574d5c8ad16b2100b
ErfanRasooli2000 <landa.cloner1379@gmail.com>
2021-12-30 17:44:22 +0330

    Slider Done
    Yeah Baby
    Slider Is Done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+let slide_now;
 function openNavs()
 {
     if(document.getElementById('nav-slider').classList.contains('navigation-slider-close'))
@@ -68,4 +69,111 @@ function showAllImges(name)
     document.getElementById('design').classList.remove('shadow');
     document.getElementById('all').classList.remove('shadow');
     document.getElementById(name).classList.add('shadow');
+}
+
+function first100()
+{
+    let pagevw = document.body.offsetWidth;
+    let marging = document.getElementById('client_slider');
+    let current = window.getComputedStyle(marging).getPropertyValue('margin-left');
+    slide_now='first100';
+   if(current!='0px')
+    {
+        backcolor('first100');
+        document.getElementById('client_slider').style.marginLeft = '0';
+    }
+}
+
+function secound100()
+{
+    let pagevw = document.body.offsetWidth;
+    let marging = document.getElementById('client_slider');
+    let current = window.getComputedStyle(marging).getPropertyValue('margin-left');
+    slide_now='secound100';
+    if(current!="-"+pagevw+"px")
+    {
+        backcolor('secound100');
+        document.getElementById('client_slider').style.marginLeft = "-"+pagevw+"px";
+    }
+}
+
+function third100()
+{
+    let pagevw = document.body.offsetWidth;
+    let marging = document.getElementById('client_slider');
+    let current = window.getComputedStyle(marging).getPropertyValue('margin-left');
+    slide_now='third100';
+    if(current!="-"+pagevw*2+"px")
+    {
+        backcolor('third100');
+        document.getElementById('client_slider').style.marginLeft = "-"+pagevw*2+"px";
+    }
+}
+
+function first50()
+{
+    let pagevw = document.body.offsetWidth;
+    let marging = document.getElementById('client_slider');
+    let current = window.getComputedStyle(marging).getPropertyValue('margin-left');
+    slide_now='first50';
+    if(current!="0")
+    {
+        backcolor('first50');
+        document.getElementById('client_slider').style.marginLeft = "0";
+    }
+}
+
+function secound50()
+{
+    let pagevw = document.body.offsetWidth;
+    let marging = document.getElementById('client_slider');
+    let current = window.getComputedStyle(marging).getPropertyValue('margin-left');
+    slide_now='secound50';
+    if(current!="-"+pagevw/2+"px")
+    {
+        backcolor('secound50');
+        document.getElementById('client_slider').style.marginLeft = "-"+pagevw/2+"px";
+    }
+}
+
+function backcolor(main)
+{
+    document.getElementById('first50').style.color='rgb(134, 151, 145)';
+    document.getElementById('secound50').style.color='rgb(134, 151, 145)';
+    document.getElementById('first100').style.color='rgb(134, 151, 145)';
+    document.getElementById('secound100').style.color='rgb(134, 151, 145)';
+    document.getElementById('third100').style.color='rgb(134, 151, 145)';
+    document.getElementById(main).style.color='rgb(89 , 89 , 89)';
+    console.log(main);
+}
+
+setInterval(slide,5000);
+function slide()
+{
+    switch (slide_now) {
+        case 'first100':
+            secound100();
+            break;
+        case 'secound100':
+            third100();
+            break;
+        case 'third100':
+            first100();
+            break;
+        case 'first50':
+            secound50();
+            break;
+        case 'secound50':
+            first50();
+            break;
+        default:
+            if (document.body.offsetWidth >= 992)
+            {
+                first50();
+            }
+            else
+            {
+                first100();
+            }
+    }
 }
